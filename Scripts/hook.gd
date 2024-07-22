64e0950bb17d5ac9cd6752c4b722b155f03c24c5
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2024-07-21 21:58:00 -0700

    up and down movement
    hook now only goes up and down

--- Scripts/hook.gd
+++ Scripts/hook.gd
@@ -11,13 +11,13 @@ func _physics_process(delta):
 	player_movement(delta)
 	
 func player_movement(_delta): 
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed 
-		velocity.y = 0 
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed 
-		velocity.y = 0 
-	elif Input.is_action_pressed("ui_down"):
+	# if Input.is_action_pressed("ui_right"):
+		# velocity.x = speed 
+		# velocity.y = 0 
+	# elif Input.is_action_pressed("ui_left"):
+		# velocity.x = -speed 
+		# velocity.y = 0 
+	if Input.is_action_pressed("ui_down"):
 		velocity.x = 0 
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
