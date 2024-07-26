7bb352d8691a806d7172730405202cc612614795
MonsieurDuck <dunstanwang@gmail.com>
2024-07-25 19:49:40 -0700

    arrow resolution fixed
    arrows and clock have correct sprites
    arrows correctly fill the screen

--- Scenes/ArrowDisplay.gd
+++ Scenes/ArrowDisplay.gd
@@ -30,19 +30,28 @@ func _process(delta):
 
 func spawnArrows(arrowArray):
 	arrowList.max_columns = arrowArray.size()
-	arrowList.size.x = arrowArray.size() * 180
-	arrowList.position.x = (get_viewport().size.x - arrowList.size.x)/2
+	arrowList.icon_scale = min(get_viewport().size.y/3.0/561.0, get_viewport().size.x/arrowArray.size()/561.0)
+	print(arrowList.icon_scale)
+	arrowList.size = Vector2(arrowArray.size() * (561 * arrowList.icon_scale + 8), get_viewport().size.y/3.0)
 	
+	var horiListSize = 0;
 	for arrow in arrowArray:
 		var arr_instance
 		if (arrow == "up"):
 			arr_instance = up
+			horiListSize += 343 * arrowList.icon_scale + 8
 		if (arrow == "down"):
 			arr_instance = down
+			horiListSize += 343 * arrowList.icon_scale + 8
 		if (arrow == "left"):
 			arr_instance = left
+			horiListSize += 561 * arrowList.icon_scale + 8
 		if (arrow == "right"):
 			arr_instance = right
+			horiListSize += 561 * arrowList.icon_scale + 8
+		
+		arrowList.size = Vector2(horiListSize + 8, get_viewport().size.y/3.0)
+		arrowList.position = Vector2((get_viewport().size.x - arrowList.size.x)/2, get_viewport().size.y * 1.0/3.0)
 		arrowList.add_icon_item(arr_instance)
 
 func changeNextArrow():
--- Scripts/gameManager.gd
+++ Scripts/gameManager.gd
@@ -15,5 +15,5 @@ func _input(event):
 	if event.is_action_pressed("comboStart"):
 		print("nice")
 		set_process_input(false)
-		await $comboView.startCombo(["up", "down", "left", "right"])
+		await $comboView.startCombo(["up", "down", "left", "right", "up", "down", "left", "right"])
 		set_process_input(true)
