9b4665e0b8b08fea605b1aa5369d096f0c0a8d45
Jason <99227586+jasoncogs@users.noreply.github.com>
2024-07-28 23:43:41 -0700

    More fish + shark
    Added all the fishies and the shark
    Fish now flip sprite when colliding
    Shark functions similar to boot for now (probably change later)

---
+++ shark.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var velo = Vector2(-300,0)
+const isShark = true
+
+func _physics_process(delta):
+	fish_movement(delta)
+
+func fish_movement(delta):
+	
+	var collision_info = move_and_collide(velo * delta)
+	# Code for detecting when fish hits hook
+	if collision_info:
+		#print(collision_info.get_collider().get("isHook"))
+		#if collision_info.get_collider().get("isHook") == true:
+			##queue_free()
+			## Stuff that happens once fish hits hook AKA start combo game here
+			#FishGlobal.emit_fish_caught()
+			#print("Fish hit hook")
+		velo = -velo
+		$Shark.flip_h = !$Shark.flip_h
