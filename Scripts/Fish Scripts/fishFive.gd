e6f62e4e11e2a2a9c0da4c9d84b741f4412ec0bd
MonsieurDuck <dunstanwang@gmail.com>
2024-07-29 16:18:57 -0700

    changed fish file structure and class hierarchy

---
+++ Scripts/Fish Scripts/fishFive.gd
@@ -0,0 +1,1 @@
+extends "res://Scripts/Fish Scripts/fish.gd"
---
+++ Scripts/Fish Scripts/shark.gd
@@ -0,0 +1,7 @@
+extends "res://Scripts/Fish Scripts/fish.gd"
+
+const isShark = true
+
+#constructor
+func _init():
+	velo = Vector2(-300,0)
