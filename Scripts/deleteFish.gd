270a613180e8606856cf85fefddba504db1b4d71
MonsieurDuck <dunstanwang@gmail.com>
2024-07-30 12:53:33 -0700

    level 2 spawning is working

---
+++ Scripts/deleteFish.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	check_collision(delta)
+	pass
+
+func check_collision(delta):
+	var collision_info = move_and_collide(Vector2(0,0), false, 0.08, true)
+	if collision_info:
+		collision_info.get_collider().destroy()
